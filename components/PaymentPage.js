9397a20165448a6bf96cbecc7185e26fd739c08c
mishrayushgit <amishra8205@gmail.com>
2025-05-06 21:03:58 +0530

    resolving payment form issue

--- components/PaymentPage.js
+++ components/PaymentPage.js
@@ -3,18 +3,21 @@ import React, { useState } from 'react'
 import Script from 'next/script'
 import Razorpay from 'razorpay'
 import { initiate } from '@/actions/useractions'
+import { useSession } from 'next-auth/react'
 const PaymentPage = ({username}) => {
+    const { data: session } = useSession()
     const [paymentform, setpaymentform] = useState({})
     const handleChange = (e) => { 
       setpaymentform({
       ...paymentform,
       [e.target.name] : [e.target.value]
       })
+      console.log(paymentform)
      }
-    const pay = async (amount,orderID) => { 
+    const pay = async (amount) => { 
         //get the order id
         let a = await initiate(amount, session?.user.name,paymentform)
-        orderID = a.id
+        let orderID = a.id
         var options = {
             "key": process.env.KEY_ID, // Enter the Key ID generated from the Dashboard
             "amount": amount, // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
@@ -85,15 +88,15 @@ const PaymentPage = ({username}) => {
     <div className='makePayment w-1/2 text-white p-5 bg-slate-900 rounded-lg'>
       <h2 className='text-xl font-bold mb-2'> Make a Payment</h2>
       <div className='flex gap-2 mt-8 flex-col justify-center'>
-        <input onChange={handleChange} value ={paymentform.name} type="text" className='w-full p-3 rounded-lg bg-slate-800' placeholder='Enter Name'/>
-        <input onChange={handleChange} value ={paymentform.message} type="text" className='w-full p-3 rounded-lg bg-slate-800' placeholder='Enter Message'/>
-        <input onChange={handleChange} value ={paymentform.amount}  type="text" className='w-full p-3 rounded-lg bg-slate-800' placeholder='Enter the Amount'/>
+        <input onChange={handleChange} value ={paymentform.name} name = "name" type="text" className='w-full p-3 rounded-lg bg-slate-800' placeholder='Enter Name'/>
+        <input onChange={handleChange} value ={paymentform.message} name = "message" type="text" className='w-full p-3 rounded-lg bg-slate-800' placeholder='Enter Message'/>
+        <input onChange={handleChange} value ={paymentform.amount} name = "amount" type="text" className='w-full p-3 rounded-lg bg-slate-800' placeholder='Enter the Amount'/>
         <button type="button" className="w-full text-white bg-gradient-to-br cursor-pointer from-pink-600 to-orange-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-100 dark:focus:ring-pink-900 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Pay</button>
       </div>
       <div className='mt-4 flex gap-5'>
-      <button className='cursor-pointer p-3 rounded-lg bg-slate-800' onClick={()=>pay(20)}>Pay ₹20</button>
-      <button className='cursor-pointer p-3 rounded-lg bg-slate-800' onClick={()=>pay(30)}>Pay ₹30</button>
-      <button className='cursor-pointer p-3 rounded-lg bg-slate-800' onClick={()=>pay(40)}>Pay ₹40</button>
+      <button className='cursor-pointer p-3 rounded-lg bg-slate-800' onClick={()=>pay(2000)}>Pay ₹20</button>
+      <button className='cursor-pointer p-3 rounded-lg bg-slate-800' onClick={()=>pay(3000)}>Pay ₹30</button>
+      <button className='cursor-pointer p-3 rounded-lg bg-slate-800' onClick={()=>pay(4000)}>Pay ₹40</button>
       </div>
     </div>
       </div>
